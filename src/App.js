95479769fefd60a19c64c8d6c9e02fe25c46f210
eyadal <31968847+eyadal@users.noreply.github.com>
2022-10-13 17:05:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  //States
+  const [pokemon, setPokemon] = useState({})
+  const [searchQuote, setSearchQuote] = useState('1')
+
+  // Sparar pokemon till ett state varje gång sidan laddas
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchQuote}`)
+      .then((res) => res.json())
+      .then((data) => setPokemon(data))
+  }, [])
+
+  // Hantera värde i sökruta i search bar
+  const handleSearch = (e) => setSearchQuote(e.target.value)
+
+  // Spara pokemon varje gång vi trycker submit
+  const getPokemonBySearch = (e) => {
+    e.preventDefault()
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchQuote}`)
+      .then((res) => res.json())
+      .then((data) => setPokemon(data))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <container class='container'>
+      <header>
+        <form onSubmit={getPokemonBySearch}>
+          <input
+            class='searchInput'
+            onChange={handleSearch}
+            type='text'
+            placeholder='Search pokemon'
+          />
+          <button class='btn'>Search</button>
+        </form>
       </header>
-    </div>
-  );
+      <main>
+        <img src={pokemon.sprites?.front_default} alt='pokemon' />
+        <h1 class='pokemonName'>{pokemon?.name}</h1>
+      </main>
+      <aside>
+        <section class='ability'>
+          <ul>
+            <p class='title'>Ability</p>
+            {pokemon.abilities?.map(({ ability }, i) => (
+              <li key={i}>{ability.name}</li>
+            ))}
+          </ul>
+        </section>
+        <section class='stats'>
+          <ul>
+            <p class='title'>Stats</p>
+            {pokemon.stats?.map(({ base_stat, stat: { name } }, i) => (
+              <li key={i}>
+                {base_stat} {name}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section class='types'>
+          <ul>
+            <p class='title'>Types</p>
+            {pokemon.types?.map(({ type: { name } }, i) => {
+              return <li key={i}>{name}</li>
+            })}
+          </ul>
+        </section>
+      </aside>
+    </container>
+  )
 }
 
-export default App;
+export default App
